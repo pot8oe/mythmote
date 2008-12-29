18430c215fe519a51835202f1e8f5a59df1c59b6
tom8oe <tom8oe@e86ebba6-d5b9-11dd-a2b5-e5f827957e07>
2008-12-29 15:52:09 +0000

    1. Updated navigation buttons to get text from string resource file. Not the quick jump buttons. These might be replaced with a list in the future.

--- trunk/src/tkj/android/homecontrol/mythmote/R.java
+++ trunk/src/tkj/android/homecontrol/mythmote/R.java
@@ -57,7 +57,17 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int down=0x7f050008;
+        public static final int fastforward=0x7f050006;
+        public static final int left=0x7f050009;
+        public static final int play=0x7f050003;
+        public static final int record=0x7f050002;
+        public static final int rewind=0x7f050005;
+        public static final int right=0x7f05000a;
+        public static final int select=0x7f05000b;
         public static final int settings_menu_str=0x7f050001;
+        public static final int stop=0x7f050004;
+        public static final int up=0x7f050007;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
