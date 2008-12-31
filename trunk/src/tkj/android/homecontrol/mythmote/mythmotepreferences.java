accf7d0632705c5d47c464cfdd2db6376c813172
tom8oe <tom8oe@e86ebba6-d5b9-11dd-a2b5-e5f827957e07>
2008-12-31 18:31:40 +0000

    1. Added initial preference loading to MythMotePreferences.onCreate.
    TODO:
    1. Implement LocationEditor.
    2. Add an onClick listener for preference clicks to show the LocationEditor.

--- trunk/src/tkj/android/homecontrol/mythmote/mythmotepreferences.java
+++ trunk/src/tkj/android/homecontrol/mythmote/mythmotepreferences.java
@@ -15,9 +15,12 @@ public class MythMotePreferences extends PreferenceActivity{
 
 	public static final int NEW_LOCATION_ID = Menu.FIRST;
 	
+	static final String SEPERATOR_PIPE_REGEX = "\\|";
 	static final String PREF_ADDRESSES = "frontend-addresses";
+	static final String PREF_NAMES = "frontend-names";
 	
-	private String[] addressesArray = null;
+	private String[] locationNames = null;
+	private String[] locationAddresses = null;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +29,39 @@ public class MythMotePreferences extends PreferenceActivity{
         //create base preference screen
         PreferenceScreen prefScreen = this.getPreferenceManager().createPreferenceScreen(this);
         
-        //load private string preference that contains all saved locations
-        String addressesStr = this.getPreferences(0).getString(PREF_ADDRESSES, null);
-        if(addressesStr != null)
+        //load preferences
+        final String nameStr = this.getPreferences(0).getString(PREF_NAMES, null);			//"Living Room|Bed Room|Kitchen";
+        final String addressesStr = this.getPreferences(0).getString(PREF_ADDRESSES, null);	//"192.168.1.101|192.168.1.102|192.168.1.3";
+        
+        if(addressesStr != null && nameStr != null)
         {
         	//split into individual addresses
-	        this.addressesArray = addressesStr.split("|");
+	        this.locationAddresses = addressesStr.split(SEPERATOR_PIPE_REGEX);
+	        this.locationNames = nameStr.split(SEPERATOR_PIPE_REGEX);
+	        
+	        if(this.locationAddresses == null || this.locationNames == null || this.locationAddresses.length != this.locationNames.length)
+	        {
+	        	//warn that settings may be corrupt
+	        	AlertDialog.Builder diag = new AlertDialog.Builder(this);
+				diag.setMessage("Settings may be corrupt!");
+				diag.setTitle("Warning!");
+				diag.setNeutralButton("OK", null);
+				diag.show();
+	        }
+	        
+	        //create preference objects and add to preference screen
+	        for(int i=0; 
+	        	i<this.locationAddresses.length && i<this.locationNames.length; 
+	        	i++)
+	        {
+	        	prefScreen.addPreference(
+	        			MythMotePreferences.createPreference(this, this.locationNames[i], this.locationAddresses[i]));
+	        }
 	        
         }
         else
         {
+        	// no preferences saved yet
         	prefScreen.addPreference(
     	        	MythMotePreferences.createPreference(this, "Create a new location", null));
         }
