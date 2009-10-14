dfb88ce4aaac0f967f3a2df60c00b76f2ea2a13c
tom8oe <tom8oe@e86ebba6-d5b9-11dd-a2b5-e5f827957e07>
2009-10-14 19:47:16 +0000

    1. Created FrontendLocation class

---
+++ trunk/src/tkj/android/homecontrol/mythmote/FrontendLocation.java
@@ -0,0 +1,15 @@
+package tkj.android.homecontrol.mythmote;
+
+public class FrontendLocation {
+
+	public String Name = "";
+	public String Address = "";
+	public int Port = 6546;
+	
+	
+	
+	public FrontendLocation()
+	{
+		
+	}
+}
