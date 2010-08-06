fc984446b5652460042595298a7f181918c8a855
tom8oe <tom8oe@e86ebba6-d5b9-11dd-a2b5-e5f827957e07>
2010-08-06 16:42:36 +0000

    1. Created branch add_user_configurable_keybindings to test robelsner's keybinding patch.

---
+++ branches/add_user_configurable_keybindings/src/tkj/android/homecontrol/mythmote/LocationChangedEventListener.java
@@ -0,0 +1,10 @@
+package tkj.android.homecontrol.mythmote;
+
+import java.util.EventListener;
+
+/**  **/
+public interface LocationChangedEventListener extends EventListener {
+	
+	public void LocationChanged();
+	
+}
