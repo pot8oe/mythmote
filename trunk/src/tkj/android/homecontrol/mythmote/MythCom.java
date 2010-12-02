7f42f4c2bbce6d9c5fa4b0a4c7247fbf61b2c446
tom8oe@gmail.com <tom8oe@gmail.com@e86ebba6-d5b9-11dd-a2b5-e5f827957e07>
2010-12-02 02:02:32 +0000

    1. Merged add_user_configurable_keybindings branch to trunk.

--- trunk/src/tkj/android/homecontrol/mythmote/MythCom.java
+++ trunk/src/tkj/android/homecontrol/mythmote/MythCom.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -24,82 +25,19 @@ import android.util.Log;
 public class MythCom {
 
 	public interface StatusChangedEventListener extends EventListener {
-		
+
 		public void StatusChanged(String StatusMsg, int statusCode);
 	}
-	
-	public static final String JUMPPOINT_channelpriorities = "channelpriorities";    //- Channel Recording Priorities
-	public static final String JUMPPOINT_channelrecpriority = "channelrecpriority";   //- Channel Recording Priorities
-	public static final String JUMPPOINT_deletebox = "deletebox";          //- TV Recording Deletion
-	public static final String JUMPPOINT_deleterecordings = "deleterecordings";    //- TV Recording Deletion
-	public static final String JUMPPOINT_flixbrowse = "flixbrowse";           //- Netflix Browser
-	public static final String JUMPPOINT_flixhistory = "flixhistory";          //- Netflix History
-	public static final String JUMPPOINT_flixqueue = "flixqueue";            //- Netflix Queue
-	public static final String JUMPPOINT_guidegrid = "guidegrid";            //- Program Guide
-	public static final String JUMPPOINT_livetv = "livetv";              //- Live TV
-	public static final String JUMPPOINT_livetvinguide = "livetvinguide";        //- Live TV In Guide
-	public static final String JUMPPOINT_mainmenu = "mainmenu";             //- Main Menu
-	public static final String JUMPPOINT_managerecordings = "managerecordings";     //- Manage Recordings / Fix Conflicts
-	public static final String JUMPPOINT_manualbox = "manualbox";            //- Manual Record Scheduling
-	public static final String JUMPPOINT_manualrecording = "manualrecording";      //- Manual Record Scheduling
-	public static final String JUMPPOINT_musicplaylists = "musicplaylists";       //- Select music playlists
-	public static final String JUMPPOINT_mythgallery = "mythgallery";          //- MythGallery
-	public static final String JUMPPOINT_mythgame = "mythgame";             //- MythGame
-	public static final String JUMPPOINT_mythmovietime = "mythmovietime";        //- MythMovieTime
-	public static final String JUMPPOINT_mythnews = "mythnews";             //- MythNews
-	public static final String JUMPPOINT_mythvideo = "mythvideo";            //- MythVideo
-	public static final String JUMPPOINT_mythweather = "mythweather";          //- MythWeather
-	public static final String JUMPPOINT_playbackbox = "playbackbox";          //- TV Recording Playback
-	public static final String JUMPPOINT_playbackrecordings = "playbackrecordings";   //- TV Recording Playback
-	public static final String JUMPPOINT_playdvd = "playdvd";              //- Play DVD
-	public static final String JUMPPOINT_playmusic = "playmusic";            //- Play music
-	public static final String JUMPPOINT_previousbox = "previousbox";          //- Previously Recorded
-	public static final String JUMPPOINT_progfinder = "progfinder";           //- Program Finder
-	public static final String JUMPPOINT_programfinder = "programfinder";        //- Program Finder
-	public static final String JUMPPOINT_programguide = "programguide";         //- Program Guide
-	public static final String JUMPPOINT_programrecpriority = "programrecpriority";   //- Program Recording Priorities
-	public static final String JUMPPOINT_recordingpriorities = "recordingpriorities";  //- Program Recording Priorities
-	public static final String JUMPPOINT_ripcd = "ripcd";                //- Rip CD
-	public static final String JUMPPOINT_ripdvd = "ripdvd";               //- Rip DVD
-	public static final String JUMPPOINT_statusbox = "statusbox";            //- Status Screen
-	public static final String JUMPPOINT_videobrowser = "videobrowser";         //- Video Browser
-	public static final String JUMPPOINT_videogallery = "videogallery";         //- Video Gallery
-	public static final String JUMPPOINT_videolistings = "videolistings";        //- Video Listings
-	public static final String JUMPPOINT_videomanager = "videomanager";         //- Video Manager
-	public static final String JUMPPOINT_viewscheduled = "viewscheduled";        //- Manage Recordings / Fix Conflicts
-	public static final String JUMPPOINT_zoneminderconsole = "zoneminderconsole";    //- ZoneMinder Console
-	public static final String JUMPPOINT_zoneminderevents = "zoneminderevents";     //- ZoneMinder Events
-	public static final String JUMPPOINT_zoneminderliveview = "zoneminderliveview";   //- ZoneMinder Live View
-	
-	public static final String KEY_up = "up";
-	public static final String KEY_down = "down";
-	public static final String KEY_left = "left";
-	public static final String KEY_right = "right";
-	public static final String KEY_enter = "enter";
-	public static final String KEY_backspace = "backspace";
-	public static final String KEY_esc = "escape";
-	
-    public static final String VOLUME_DOWN = "[";
-    public static final String VOLUME_UP = "]";
-    public static final String VOLUME_MUTE = "|";
-    public static final String CH_RETURN = "h";
-	public static final String PLAY_CH_UP = "channel up";
-	public static final String PLAY_CH_DW = "channel down";
-	public static final String PLAY_STOP = "stop";
-	public static final String PLAY_PLAY = "speed normal";
-	public static final String PLAY_SEEK_FW = "seek forward";
-	public static final String PLAY_SEEK_BW = "seek backward";
-	public static final String EXIT = "exit";
-	
+
 	public static final int DEFAULT_MYTH_PORT = 6546;
-	public static final int SOCKET_TIMEOUT = 10000;
+	public static final int SOCKET_TIMEOUT = 2000;
 	public static final int ENABLE_WIFI = 0;
 	public static final int CANCEL = 1;
 	public static final int STATUS_DISCONNECTED = 0;
 	public static final int STATUS_CONNECTED = 1;
 	public static final int STATUS_CONNECTING = 3;
 	public static final int STATUS_ERROR = 99;
-	
+
 	private static Timer _timer;
 	private static Toast _toast;
 	private static Socket _socket;
@@ -111,7 +49,7 @@ public class MythCom {
 	private static int _statusCode;
 	private static StatusChangedEventListener _statusListener;
 	private static FrontendLocation _frontend;
-	
+
 	private final Handler mHandler = new Handler();
 	private final Runnable mSocketActionComplete = new Runnable()
 	{
@@ -121,7 +59,7 @@ public class MythCom {
 			if(_statusCode!=STATUS_CONNECTING)
 			    _toast.cancel();
 		}
-		
+
 	};
 	
 	/** TimerTask that probes the current connection for its mythtv screen.  **/
@@ -148,7 +86,7 @@ public class MythCom {
 		//get connection manager
 		_conMgr = (ConnectivityManager) _parent.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-		//set address and port
+		// set address and port
 		_frontend = frontend;
 
 		//create toast for all to eat and enjoy
@@ -158,7 +96,7 @@ public class MythCom {
 
 		this.setStatus("Connecting", STATUS_CONNECTING);
 
-		//create a socket connecting to the address on the requested port
+		// create a socket connecting to the address on the requested port
 		this.connectSocket();
 	}
 	
@@ -171,18 +109,16 @@ public class MythCom {
 			//send exit if connected
 			if(this.IsConnected())
 				this.sendData("exit\n");
-			
-			//check if output stream exists
-			if(_outputStream != null)
-			{
+
+			// check if output stream exists
+			if (_outputStream != null) {
 				_outputStream.close();
 				_outputStream = null;
 			}
-			
-			//check if input stream exists
-			if(_inputStream != null)
-			{
-				//close input stream
+
+			// check if input stream exists
+			if (_inputStream != null) {
+				// close input stream
 				_inputStream.close();
 				_inputStream = null;
 			}
@@ -201,44 +137,42 @@ public class MythCom {
 			this.setStatus("Disconnect I/O error", STATUS_ERROR);
 		}
 	}
-	
-	public void SendJumpCommand(String jumpPoint)
-	{
-		//send command data
+
+	public void SendCommand(String jumpPoint) {
+		// send command data
+		this.sendData(String.format("%s\n", jumpPoint));
+	}
+
+	public void SendJumpCommand(String jumpPoint) {
+		// send command data
 		this.sendData(String.format("jump %s\n", jumpPoint));
 	}
-	
-	public void SendKey(String key)
-	{
-		//send command data
+
+	public void SendKey(String key) {
+		// send command data
 		this.sendData(String.format("key %s\n", key));
 	}
-	
-	public void SendKey(char key)
-	{
-		//send command data
+
+	public void SendKey(char key) {
+		// send command data
 		this.sendData(String.format("key %s\n", key));
 	}
-	
-	public void SendPlaybackCmd(String cmd)
-	{
-		//send command data
+
+	public void SendPlaybackCmd(String cmd) {
+		// send command data
 		this.sendData(String.format("play %s\n", cmd));
 	}
-	
-	public void SetOnStatusChangeHandler(StatusChangedEventListener listener)
-	{
+
+	public void SetOnStatusChangeHandler(StatusChangedEventListener listener) {
 		_statusListener = listener;
 	}
-	
-	public String GetStatusStr()
-	{
+
+	public String GetStatusStr() {
 		return _status;
 	}
-	
-	public boolean IsNetworkReady()
-	{
-		if(_conMgr != null && _conMgr.getActiveNetworkInfo().isConnected())
+
+	public boolean IsNetworkReady() {
+		if (_conMgr != null && _conMgr.getActiveNetworkInfo().isConnected())
 			return true;
 		return false;
 	}
@@ -316,12 +250,13 @@ public class MythCom {
 						}
 					}
 				}
-				
-				//post results
+
+				// post results
 				mHandler.post(mSocketActionComplete);
 			}
 		};
-		
+
+		// run thread
 		thread.start();
 	}
 	
@@ -391,10 +326,10 @@ public class MythCom {
 			public void run() 
 			{
 				_status = StatusMsg;
-				if(_statusListener != null)
+				if (_statusListener != null)
 					_statusListener.StatusChanged(StatusMsg, code);
 			}
-			
+
 		});
 	}
 	
