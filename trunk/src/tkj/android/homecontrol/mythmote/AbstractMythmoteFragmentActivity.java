8dd9c2ca2ec36b0edcb2a2e0ba2edbfceb4ec34f
tom8oe@gmail.com <tom8oe@gmail.com@e86ebba6-d5b9-11dd-a2b5-e5f827957e07>
2014-01-02 16:58:45 +0000

    * Added restarting Mythmote when preference theme does not match current application theme.

--- trunk/src/tkj/android/homecontrol/mythmote/AbstractMythmoteFragmentActivity.java
+++ trunk/src/tkj/android/homecontrol/mythmote/AbstractMythmoteFragmentActivity.java
@@ -1,11 +1,13 @@
 package tkj.android.homecontrol.mythmote;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class AbstractMythmoteFragmentActivity extends FragmentActivity {
-
 	
+	private static int sCurrentTheme = -1;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -15,12 +17,30 @@ public class AbstractMythmoteFragmentActivity extends FragmentActivity {
 		
 	}
 	
-	private void setApplicationTheme() {
+	@Override
+	protected void onResume() {
+		super.onResume();
 		
 		int theme = this.getSharedPreferences(MythMotePreferences.MYTHMOTE_SHARED_PREFERENCES_ID, MODE_PRIVATE)
 				.getInt(MythMotePreferences.PREF_APP_THEME, 0);
 		
-		this.setTheme(AbstractMythmoteFragmentActivity.getThemeStyle(theme));
+		if(sCurrentTheme != -1 && theme != sCurrentTheme){
+			Intent i = new Intent("android.intent.action.MAIN");
+			i.setComponent(ComponentName.unflattenFromString("tkj.android.homecontrol.mythmote/tkj.android.homecontrol.mythmote.MythMote"));
+			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(i);
+		}
+	}
+	
+	
+	
+	
+	private void setApplicationTheme() {
+		
+		sCurrentTheme = this.getSharedPreferences(MythMotePreferences.MYTHMOTE_SHARED_PREFERENCES_ID, MODE_PRIVATE)
+				.getInt(MythMotePreferences.PREF_APP_THEME, 0);
+		
+		this.setTheme(AbstractMythmoteFragmentActivity.getThemeStyle(sCurrentTheme));
 	}
 	
 	public static int getThemeStyle(int preferenceValue){
