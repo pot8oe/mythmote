1812656e9a32c85e80b5b3aca34e44326fdee092
Thomas G. Kenny Jr <tom8oe@gmail.com>
2015-09-26 17:48:27 -0400

    * Switched application to AppCompat Theme
    * Commented out custom button styles

--- app/src/main/java/tkj/android/homecontrol/mythmote/AbstractMythmoteFragmentActivity.java
+++ app/src/main/java/tkj/android/homecontrol/mythmote/AbstractMythmoteFragmentActivity.java
@@ -4,8 +4,9 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public class AbstractMythmoteFragmentActivity extends FragmentActivity {
+public class AbstractMythmoteFragmentActivity extends AppCompatActivity {
 	
 	private static int sCurrentTheme = -1;
 	
